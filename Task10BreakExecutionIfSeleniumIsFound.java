282bab04894a928383fb43bc55c11351901507c5
Rejeesh86 <115663206+Rejeesh86@users.noreply.github.com>
2022-10-17 16:12:36 -0400

    Update Task10BreakExecutionIfSeleniumIsFound.java
    Made correction in the code. initial understanding of the question was different. Corrected after hearing the doubt session.

--- Task10BreakExecutionIfSeleniumIsFound.java
+++ Task10BreakExecutionIfSeleniumIsFound.java
@@ -1,23 +1,21 @@
 package assignment1;
 
-import java.util.Scanner;
-
-public class Task10BreakExecutionIfSeleniumIsFound 
+public class Task10BreakExecutionIfSeleniumIsFound2Updated 
 {
 
 	public static void main(String[] args)
 	{	
 		
-		Scanner sc = new Scanner(System.in);
-		String input;
-		System.out.println("Program will break the current execution untill user inputs selenium");	
-		do
+		String input [] = {"Java","JavaScript","Selenium","Python","Mukesh"};
+		for (int i=0; i<input.length;i++)
 		{
-			System.out.println("Enter your input");
-			input = sc.next();
-			
-		}while (!input.equalsIgnoreCase("Selenium"));
-			
-				System.out.println("Entered input is Selenium. Execution breaking");
+			if(!input[i].equals("Selenium"))
+			{
+				System.out.println(input[i]);
+			}
+			else
+				break;
+		}
+	
 	}
 }
